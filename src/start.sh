cf03aadfb911b296abb3df403b41c4e76de9ba68
Diego Escobar <diego@sisutech.ee>
2023-08-29 14:16:11 +0300

    added multi services

--- src/start.sh
+++ src/start.sh
@@ -6,35 +6,58 @@ packages_path="packages/"
 # Array of extensions to watch
 watch_extensions=("mts" "json" "ts")
 
-# Select the service
-echo "Select service to work on:"
+# clean up telepresence
+telepresence quit
+
+# Select the services
+echo "Select services to work on:"
 cd $base_path/backend/
+
 select service_name in $(ls -d */); do
-    break
+    if [ -n "$service_name" ]; then
+        service_names+=("$service_name")
+        echo ""
+        echo "${service_names[@]}"
+        echo ""
+        echo "Do you want to select another service? (y/N)"
+        read answer
+        if [ "$answer" != "y" ]; then
+            break
+        fi
+    else
+        echo "Invalid selection"
+    fi
 done
 
-cd $base_path/backend/$service_name
-pnpm run start:telepresence &
-PID_TELEPRESENCE=$!
+for service_name in "${service_names[@]}"; do
+    cd $base_path/backend/$service_name
+    pnpm run start:telepresence &
+done
 
 # function commands to execute on each change
 # $1: package absolute path
 function on_change_package {
     local package_path=$1
 
-    # Stopping telepresence if running
-    if [ ! -z $PID_TELEPRESENCE ] && kill -0 $PID_TELEPRESENCE 2>/dev/null; then
-        echo "Killing telepresence"
-        kill -SIGINT $PID_TELEPRESENCE
-    fi
-
     cd $package_path
     pnpm run build
 
+    echo "Restarting telepresence"
+
     # Restarting telepresence
-    cd $base_path/backend/$service_name
-    pnpm run start:telepresence &
-    PID_TELEPRESENCE=$!
+    for service_name in "${service_names[@]}"; do
+
+        # restart telepresence making a change in the main.mts file
+        local app_file_path="$base_path""backend/$service_name""src/app.mts"
+
+        echo modifiying $app_file_path
+
+        # add empty line at the end of src/app.mts
+        echo -e "\nconsole.log();" >>$app_file_path
+        sleep 1
+        # remove empty lines at the end of src/app.mts
+        head -n $(($(wc -l <$app_file_path) - 2)) $app_file_path >temp.txt && mv temp.txt $app_file_path
+    done
 }
 
 regex_ext=$(printf "|%s" "${watch_extensions[@]}")
